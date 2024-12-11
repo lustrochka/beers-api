85bde8d593425ac96fbcd59d645a5864c2003789
Olga <kartoshkaslukom@gmail.com>
2024-12-11 20:34:58 +0100

    feat: add endpoints

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import http, { IncomingMessage, ServerResponse } from 'http';
+import { getBeer } from './handlers/getBeer';
+import { getBeers } from './handlers/getBeers';
+
+const baseUrl = '/api/beers';
+
+const requestHandler = (req: IncomingMessage, res: ServerResponse) => {
+  const { method, url } = req;
+
+  if (!url) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Invalid URL');
+  } else {
+    try {
+      if (url === baseUrl && method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        const message = getBeers(req);
+        res.end(JSON.stringify(message));
+      } else if (url?.startsWith(baseUrl + '/') && method === 'GET') {
+        const id = url.split('/')[3];
+        const { code, message, contentType } = getBeer(id);
+        res.writeHead(code, { 'Content-Type': contentType });
+        res.end(message);
+      } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not found');
+      }
+    } catch {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    }
+  }
+};
+
+export const server = http.createServer(requestHandler);
---
+++ src/utils/convertToJson.ts
@@ -0,0 +1,33 @@
+import csv from 'csvtojson';
+import fs from 'fs/promises';
+import { v4 } from 'uuid';
+
+const csvPath = 'src/data/data.csv';
+const jsonPath = 'src/data/data.json';
+
+async function convertToJson() {
+  try {
+    const fileHandle: fs.FileHandle = await fs.open(jsonPath, 'wx');
+
+    const data = await csv().fromFile(csvPath);
+    const jsonArray = data.map((item) => {
+      const { name, abv, ibu, type, country } = item;
+
+      return {
+        id: v4(),
+        name,
+        abv,
+        ibu,
+        type,
+        country,
+      };
+    });
+
+    await fileHandle.write(JSON.stringify(jsonArray, null, 2));
+    await fileHandle?.close();
+  } catch (error) {
+    console.error('Ошибка при работе с файлом:', error);
+  }
+}
+
+convertToJson();
