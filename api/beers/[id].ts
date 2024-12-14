ad16318a398c7910fb351ae06d61684e46ab95a5
olga <94057960+lustrochka@users.noreply.github.com>
2024-12-14 23:53:25 +0100

    Update [id].ts

--- api/beers/[id].ts
+++ api/beers/[id].ts
@@ -4,7 +4,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 export async function handler(req: VercelRequest, res: VercelResponse) {
   //const { method, url } = req;
 
-  res.status(200).json({r: req});
+  res.status(200).json({r: req.url});
 
   /*if (!url) {
     res.status(404).send('Invalid URL');
