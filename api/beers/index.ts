c19f7bcb8e30aab7eb5bdbfd5a86eee02c017e5c
olga <94057960+lustrochka@users.noreply.github.com>
2024-12-14 21:26:34 +0100

    Update index.ts

--- api/beers/index.ts
+++ api/beers/index.ts
@@ -1,7 +1,7 @@
 import { getBeers } from '../../handlers/getBeers';
-import { IncomingMessage, ServerResponse } from 'http';
+import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export async function handler(res: ServerResponse, req: IncomingMessage) {
+export async function handler(res: VercelResponse, req: VercelRequest) {
   console.log(req);
   if (req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
