3d88898e5c086c4a5f6b2a6f9aeb16d0603837e3
olga <94057960+lustrochka@users.noreply.github.com>
2024-12-14 21:41:35 +0100

    Update getBeers.ts

--- handlers/getBeers.ts
+++ handlers/getBeers.ts
@@ -2,8 +2,14 @@ import beers from '../data/data.json';
 import { IncomingMessage } from 'http';
 import { parse } from 'url';
 
+const products = [
+    { id: 1, name: 'Laptop', price: 1000 },
+    { id: 2, name: 'Smartphone', price: 500 },
+    { id: 3, name: 'Tablet', price: 300 },
+];
+
 export function getBeers(req: IncomingMessage) {
-  const { query } = parse(req.url || '', true);
+  /*const { query } = parse(req.url || '', true);
   const { page, pageSize, search } = query;
 
   const parsedPage = Number(page) || 1;
@@ -16,5 +22,6 @@ export function getBeers(req: IncomingMessage) {
   const startIndex = parsedPageSize * (parsedPage - 1);
   const resultBeers = beersArray.slice(startIndex, startIndex + parsedPageSize);
 
-  return { message: resultBeers, total: beersArray.length };
+  return { message: resultBeers, total: beersArray.length };*/
+  return products;
 }
