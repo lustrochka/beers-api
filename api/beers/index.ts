dd4ca954a01143b9ff2444d85bf2c0e484473ed4
olga <94057960+lustrochka@users.noreply.github.com>
2024-12-14 21:03:19 +0100

    Update index.ts

--- api/beers/index.ts
+++ api/beers/index.ts
@@ -2,6 +2,7 @@ import { getBeers } from '../../handlers/getBeers';
 import { IncomingMessage, ServerResponse } from 'http';
 
 export async function handler(res: ServerResponse, req: IncomingMessage) {
+  console.log(req);
   if (req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     const message = getBeers(req);
