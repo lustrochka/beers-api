e5995ea5203dd19d5975faad090c4dc403fa5d4b
olga <94057960+lustrochka@users.noreply.github.com>
2024-12-14 21:53:38 +0100

    Update [id].ts

--- api/beers/[id].ts
+++ api/beers/[id].ts
@@ -1,4 +1,4 @@
-import { getBeer } from '../../handlers/getBeer';
+/*import { getBeer } from '../../handlers/getBeer';
 import { IncomingMessage, ServerResponse } from 'http';
 
 export async function handler(res: ServerResponse, req: IncomingMessage) {
@@ -15,4 +15,20 @@ export async function handler(res: ServerResponse, req: IncomingMessage) {
       res.end(message);
     }
   }
+}*/
+
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+const products = [
+    { id: 1, name: 'Laptop', price: 1000 },
+    { id: 2, name: 'Smartphone', price: 500 },
+    { id: 3, name: 'Tablet', price: 300 },
+];
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+    if (req.method === 'GET') {
+        res.status(200).json(products);
+    } else {
+        res.status(405).json({ error: 'Method Not Allowed' });
+    }
 }
