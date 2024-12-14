29dc08bb943903bf7ce73844ec49596291c868ab
olga <94057960+lustrochka@users.noreply.github.com>
2024-12-14 21:49:58 +0100

    Update index.ts

--- api/beers/index.ts
+++ api/beers/index.ts
@@ -1,9 +1,15 @@
 import { getBeers } from '../../handlers/getBeers';
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
+const products = [
+    { id: 1, name: 'Laptop', price: 1000 },
+    { id: 2, name: 'Smartphone', price: 500 },
+    { id: 3, name: 'Tablet', price: 300 },
+];
 export async function handler(res: VercelResponse, req: VercelRequest) {
   if (req.method === 'GET') {
-    const message = getBeers(req);
-    res.status(200).json(message);
+    /*const message = getBeers(req);*/
+    res.status(200).json(products);
   }
 }
+
