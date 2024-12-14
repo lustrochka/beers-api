482d621f09b255f102de350208d69e5489a769e7
olga <94057960+lustrochka@users.noreply.github.com>
2024-12-15 00:23:22 +0100

    Update [id].ts

--- api/beers/[id].ts
+++ api/beers/[id].ts
@@ -2,9 +2,8 @@ import { getBeer } from '../../handlers/getBeer';
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-
-    if (method === 'GET') {
-      const { code, message, contentType } = getBeer(req.query.id);
+    if (req.method === 'GET') {
+      const { code, message } = getBeer(req.query.id as string);
       res.status(code).send(message);
   }
 }
