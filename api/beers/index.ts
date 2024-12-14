e475704667b6867d0dfd7e1fb18131fa7daf30f2
olga <94057960+lustrochka@users.noreply.github.com>
2024-12-14 22:09:10 +0100

    Update index.ts

--- api/beers/index.ts
+++ api/beers/index.ts
@@ -4,6 +4,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 export default function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'GET') {
     const message = getBeers(req);
+    res.status(200).json(message);
   }
 }
 
