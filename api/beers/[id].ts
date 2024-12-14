504df22bf51bd4d1f8dd4e27f0277d3da14a9670
olga <94057960+lustrochka@users.noreply.github.com>
2024-12-15 00:13:03 +0100

    Update [id].ts

--- api/beers/[id].ts
+++ api/beers/[id].ts
@@ -2,18 +2,9 @@ import { getBeer } from '../../handlers/getBeer';
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  //const { method, url } = req;
 
-  const { id } = req.query;
-  res.status(200).json({r: id});
-
-  /*if (!url) {
-    res.status(404).send('Invalid URL');
-  } else {
     if (method === 'GET') {
-      const id = url.split('/')[3];
-      const { code, message, contentType } = getBeer(id);
+      const { code, message, contentType } = getBeer(req.query.id);
       res.status(code).send(message);
-    }
-  }*/
+  }
 }
